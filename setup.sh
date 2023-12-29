c447c64974dde21330a14922cde21106a07e4791
Prince527 <Prince527gaming@gmail.com>
2023-12-29 09:44:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -e /app/config/config.toml ]; then
-    ./playit-linux-amd64 setup -s --secret_path /app/config/config.toml
+    ./playit-linux-amd64 setup --secret_path /app/config/config.toml -s
 else
-    ./playit-linux-amd64 -s --secret_path /app/config/config.toml
+    ./playit-linux-amd64 --secret_path /app/config/config.toml -s
 fi
