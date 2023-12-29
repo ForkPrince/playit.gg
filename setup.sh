f93314121a652082300c2c7811808b399a7b66f4
Prince527 <Prince527gaming@gmail.com>
2023-12-29 09:40:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 if [ ! -e /app/config.toml ]; then
-    echo "-- Setup playit agent --"
-    echo "-- You may need to restart the container after --"
-
     ./playit-linux-amd64 setup -s --secret_path /app/config.toml
 else
     ./playit-linux-amd64 -s --secret_path /app/config.toml
