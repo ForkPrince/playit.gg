889c1a7c4d3739aeb838af0dc2c5b0a59031e8cc
Prince527 <Prince527gaming@gmail.com>
2023-12-29 09:12:29 -0400

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,8 @@
+if [ ! -e /app/config.toml ]; then
+    echo "-- Setup playit agent --"
+    echo "-- You may need to restart the container after --"
+
+    ./playit-linux-amd64 setup -s
+else
+    ./playit-linux-amd64 -s
+fi
